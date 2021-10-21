49f56ee62ada6927a3e421d9b6265b7c37237790
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-10-21 14:22:39 +0200

    first simple version

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Point-Cluster-Detection
+# Point Cluster Detection
+
+MoveApps
+
+Github repository: *github.com/movestore/Point-Cluster_Detection*
+
+## Description
+Detection of point clusters, where possibly more than one animal returns to within a specified time interval. Provides a table of each cluster with the times, duration, number of locations and animals.
+
+## Documentation
+This App uses hierarchical clustering for the detection of point clusters where one or more animals return to repeatedly within a specified time frame. For clustering the Ward methods is used and the clusters are defined at the minimum distance between cluster centres of `2 * cluster radius`. Only clusters that were used for at least the specified number of hours/days/weeks are returned.
+
+A cluster overview table is returned as a .csv artefact to download. It included for each cluster the mid location, timestamps of first and last location, duration, number of locations, number of animals and the names of those animals.
+
+The output of the App includes only the locations that could be attributed to a cluster that fulfilled the minimum duration requirement. This dataset is also returned as a .csv artefact to download.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+
+### Artefacts
+`Cluster_Table.csv`: Overview of properties of detected point clusters.
+
+`Points_With_Clusters.csv`: Result data set as .csv, with all locations in clusters.
+
+### Parameters 
+`rad`: Radius within which locations have to lie to be defined as a cluster. Unit = metre. Default 200 m.
+
+`dur`: Duration that a cluster has to be repeatadly visited. Unit below. Default 14.
+
+`dur_unit`: Duration unit for variable `dur`. Can be `hours`, `days` or `weeks`. Default `days`.
+
+### Null or error handling:
+**Parameter `rad`:** Radius NULL defaults to 200 m. Too small radii might lead to small clusters, please include location inaccuracies here.
+
+**Parameter `dur`:** Duration NULL defaults to 14 (days). Too large durations might lead to few clusters.
+
+**Parameter `dur_unit`:** Duration defaults to `days`. Only regular time units can be used (see above).
+
+**Data:** All locations that are in a (any) cluster are returned to the next App. If no clusters are found in your data set NULL is returned, likel with an error.
---
+++ RFunction.R
@@ -0,0 +1,67 @@
+library('move')
+library('geodist')
+
+rFunction = function(rad=NULL, dur=NULL, dur_unit="days", data, ...) {
+  
+  Sys.setenv(tz="UTC") 
+  
+  if (is.null(rad))
+  {
+    logger.info("Your cluster radius is not supplied. We use default 200 m.")
+    rad <- 200
+  }
+  if (is.null(dur))
+  {
+    logger.info(paste0("Your minimum cluster duration is not supplied. We use 14 (",dur_unit,")  as default."))
+    dur <- 14
+  }
+  
+  # cluster for all locations (not by ID)
+  coos <- coordinates(data)
+  dista <- geodist_vec(x1=coos[,1],y1=coos[,2],measure="vincenty") #unit=m, "geodesic" is probably better, but takes even longer
+  
+  clu <- hclust(as.dist(dista),method="ward.D2")
+  memb <- cutree(clu,h=2*rad) #group membership for each location
+  #plot(as.dendrogram(clu), ylim = c(0,1000))
+  
+  data@data <- cbind(data@data,"clusterID"=memb)
+  cluID_all <- unique(memb)
+  
+  cluID <- apply(matrix(cluID_all), 1, function(x) ifelse(as.numeric(difftime(max(timestamps(data)[data@data$clusterID==x]),min(timestamps(data)[data@data$clusterID==x]),unit=dur_unit))>=dur, x, NA))
+  
+  cluID <- cluID[!is.na(cluID)]
+  
+  if (length(cluID)>0)
+  {
+    result <- data[data@data$clusterID %in% cluID] #these are all locations that are in a cluster with difftime>dur
+    midlon <- apply(matrix(cluID), 1, function(x) mean(coordinates(data[data@data$clusterID==x])[,1])) 
+    midlat <- apply(matrix(cluID), 1, function(x) mean(coordinates(data[data@data$clusterID==x])[,2])) 
+    
+    result@data$cluster.mid.long <- apply(matrix(result@data$clusterID), 1, function(x) midlon[which(cluID==x)])
+    result@data$cluster.mid.lat <- apply(matrix(result@data$clusterID), 1, function(x) midlat[which(cluID==x)])
+    result.df <- as.data.frame(result)
+    clu.ix <- which(names(result.df) %in% c("clusterID","cluster.mid.long","cluster.mid.lat"))
+    result.df <- data.frame(result.df[,clu.ix],result.df[,-clu.ix])
+    #write.csv(result.df,file=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"Points_With_Clusters.csv"),row.names=FALSE)
+    write.csv(result.df,file="Points_With_Clusters.csv",row.names=FALSE) 
+    
+    #cluster table
+    n.locs <- apply(matrix(cluID), 1, function(x) length(which(result@data$clusterID==x)))
+    n.ids <- apply(matrix(cluID), 1, function(x) length(unique(result.df$trackId[result.df$clusterID==x])))
+    id.names <- apply(matrix(cluID), 1, function(x) paste(unique(result.df$trackId[result.df$clusterID==x]),collapse=", "))
+    timestamp.start <- apply(matrix(cluID), 1, function(x) paste(as.character(min(timestamps(result[result@data$clusterID==x]))),"UTC"))
+    timestamp.end <- apply(matrix(cluID), 1, function(x) paste(as.character(max(timestamps(result[result@data$clusterID==x]))),"UTC"))
+    duration <- as.numeric(difftime(as.POSIXct(timestamp.end), as.POSIXct(timestamp.start),units=dur_unit))
+    
+    
+    clu_tab <- data.frame("cluster.ID"=cluID,"mid.long"=midlon,"mid.lat"=midlat,timestamp.start,timestamp.end,duration,n.locs,n.ids,id.names)
+    names(clu_tab)[names(clu_tab)=="duration"] <- paste0("duration (",dur_unit,")")
+    #write.csv(clu_tab,file=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"Cluster_Table.csv"),row.names=FALSE)
+    write.csv(clu_tab,file="Cluster_Table.csv",row.names=FALSE)
+  } else result <- NULL
+  
+  # could add n.locs and duration for different individuals
+  # add another option to use package recurse
+  
+  return(result)
+}
